eaf0e38c95b3331dab0d39d085c3f42c49df75b1
koudayao27 <113324030+koudayao27@users.noreply.github.com>
2022-10-18 13:37:37 +0800

    remove useless code

---
+++ dht11.ts
@@ -0,0 +1,116 @@
+/**
+ * MakeCode editor extension for DHT11 humidity/temperature sensors
+ * by Alan Wang
+ */
+
+enum dataType {
+    //% block="humidity"
+    humidity,
+    //% block="temperature"
+    temperature,
+}
+
+//% block="DHT11" weight=100 color=#ff8f3f icon="\uf043"
+namespace DHT11 {
+
+    let _temperature: number = -999.0
+    let _humidity: number = -999.0
+    let _readSuccessful: boolean = false
+    let _sensorresponding: boolean = false
+
+    /**
+    * Query data from DHT11 sensor. If you are using 4 pins/no PCB board versions, you'll need to pull up the data pin. 
+    * It is also recommended to wait 1 second between each query.
+    */
+    //% block="Query1 Data pin $dataPin"
+    export function queryData(dataPin: DigitalPin) {
+        //initialize
+        let startTime: number = 0
+        let endTime: number = 0
+        let checksum: number = 0
+        let checksumTmp: number = 0
+        let dataArray: boolean[] = []
+        let resultArray: number[] = []
+
+        for (let index = 0; index < 40; index++) dataArray.push(false)
+        for (let index = 0; index < 5; index++) resultArray.push(0)
+
+        _humidity = -999.0
+        _temperature = -999.0
+        _readSuccessful = false
+        _sensorresponding = false
+        startTime = input.runningTimeMicros()
+
+        //request data
+        pins.digitalWritePin(dataPin, 0) //begin protocol, pull down pin
+        basic.pause(18)
+
+        pins.setPull(dataPin, PinPullMode.PullUp) //pull up data pin if needed
+        pins.digitalReadPin(dataPin) //pull up pin
+        control.waitMicros(40)
+
+        if (pins.digitalReadPin(dataPin) == 1) {
+
+        } else {
+
+            _sensorresponding = true
+
+            while (pins.digitalReadPin(dataPin) == 0); //sensor response
+            while (pins.digitalReadPin(dataPin) == 1); //sensor response
+
+            //read data (5 bytes)
+            for (let index = 0; index < 40; index++) {
+                while (pins.digitalReadPin(dataPin) == 1);
+                while (pins.digitalReadPin(dataPin) == 0);
+                control.waitMicros(28)
+                //if sensor still pull up data pin after 28 us it means 1, otherwise 0
+                if (pins.digitalReadPin(dataPin) == 1) dataArray[index] = true
+            }
+
+            endTime = input.runningTimeMicros()
+
+            //convert byte number array to integer
+            for (let index = 0; index < 5; index++)
+                for (let index2 = 0; index2 < 8; index2++)
+                    if (dataArray[8 * index + index2]) resultArray[index] += 2 ** (7 - index2)
+
+            //verify checksum
+            checksumTmp = resultArray[0] + resultArray[1] + resultArray[2] + resultArray[3]
+            checksum = resultArray[4]
+            if (checksumTmp >= 512) checksumTmp -= 512
+            if (checksumTmp >= 256) checksumTmp -= 256
+            if (checksum == checksumTmp) _readSuccessful = true
+
+            //read data if checksum ok
+            if (_readSuccessful) {
+                _humidity = resultArray[0] + resultArray[1] / 100
+                _temperature = resultArray[2] + resultArray[3] / 100
+            }
+        }
+    }
+
+    /**
+    * Read humidity/temperature data from lastest query of DHT11
+    */
+    //% block="Read $data"
+    export function readData(data: dataType): number {
+        return data == dataType.humidity ? _humidity : _temperature
+    }
+
+    /**
+    * Determind if last query is successful (checksum ok)
+    */
+    //% block="Last query successful?"
+    export function readDataSuccessful(): boolean {
+        return _readSuccessful
+    }
+
+    /**
+    * Determind if sensor responded successfully (not disconnected, etc) in last query
+    */
+    //% block="Last query sensor responding?" advanced=true
+    export function sensorrResponding(): boolean {
+        return _sensorresponding
+    }
+
+}
--- test.ts
+++ test.ts
@@ -1,11 +1,5 @@
-DHT11_DHT22.queryData(
-DHTtype.DHT11,
-DigitalPin.P0,
-true,
-true,
-true
-)
-if (DHT11_DHT22.readDataSuccessful()) {
-    basic.showString("T: " + ("" + DHT11_DHT22.readData(dataType.temperature)).substr(0, 2))
-    basic.showString("H: " + ("" + DHT11_DHT22.readData(dataType.humidity)).substr(0, 2))
+DHT11.queryData(DigitalPin.P15)
+if (DHT11.readDataSuccessful()) {
+    basic.showString("T: " + ("" + DHT11.readData(dataType.temperature)).substr(0, 2))
+    basic.showString("H: " + ("" + DHT11.readData(dataType.humidity)).substr(0, 2))
 }
